b233e1f746fdb0ea4983ef40a7d96e3a1411eca2
Abhinav Tushar <abhinav@lepisma.xyz>
2020-03-29 02:36:30 +0530

    basic cli for grapheme distortion

---
+++ distort/cli.py
@@ -0,0 +1,31 @@
+"""
+Tool for lexical distortion. Takes input from stdin and dumps on stdout.
+
+Usage:
+  distort grapheme (random) [--p=<p>]
+
+Options:
+  --p=<p>                Perturbation probability [default: 0.2]
+"""
+
+import sys
+
+from docopt import docopt
+from pydash import py_
+
+from distort.distort import replace_with_p
+
+
+def main():
+    args = docopt(__doc__)
+
+    if args["grapheme"]:
+        if args["random"]:
+            p = float(args["--p"])
+
+            pieces = [[list(word) for word in line.split()] for line in sys.stdin]
+            unique_pieces = set(py_.flatten_deep(pieces))
+
+            for line in pieces:
+                d_words = ["".join(replace_with_p(word, unique_pieces, p=p)) for word in line]
+                print(" ".join(d_words))
